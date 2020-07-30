1c544cae3dd8d4d26936db69ac87dc1619136072
Amar Patel <amar1patel84@gmail.com>
2020-07-30 13:43:14 -0500

    Visuals

--- Vaporwave/Assets/ScriptFolder/ParamCube.cs
+++ Vaporwave/Assets/ScriptFolder/ParamCube.cs
@@ -10,6 +10,7 @@ public class ParamCube : MonoBehaviour
     public bool useBuffer;
     public bool isSpeaker;
     public bool sizeChange;
+    public bool rotation;
     [SerializeField] Material material;
     // Start is called before the first frame update
     void Start()
@@ -28,6 +29,8 @@ public class ParamCube : MonoBehaviour
             transform.localScale = new Vector3(transform.localScale.x, (AudioVisual.bandBuffer[band] * scaleMultiplier ) + startScale, transform.localScale.z);
           } else if (isSpeaker && sizeChange){
             transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y , -1f * (AudioVisual.bandBuffer[band] * scaleMultiplier) + startPos);
+          } else if (rotation){
+            transform.localRotation = Quaternion.Euler(transform.localRotation.x + AudioVisual.bandBuffer[band] * 20, transform.localRotation.y, transform.localRotation.z);
           }
           Color color = new Color (AudioVisual.audioBandBuffer[band], AudioVisual.audioBandBuffer[band], AudioVisual.audioBandBuffer[band]);
           material.SetColor("_EmissionColor", color);
@@ -38,6 +41,8 @@ public class ParamCube : MonoBehaviour
           }
           else if (isSpeaker && sizeChange) {
             transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y , -1f * (AudioVisual.freqBand[band] * scaleMultiplier) + startPos);
+          } else if (rotation){
+            transform.localRotation = Quaternion.Euler(transform.localRotation.x + AudioVisual.freqBand[band] * 20, transform.localRotation.y, transform.localRotation.z);
           }
           Color color = new Color (AudioVisual.audioBand[band], AudioVisual.audioBand[band], AudioVisual.audioBand[band]);
           material.SetColor("_EmissionColor", color);
