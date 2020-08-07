96ded6202a9cc34aa8f82b5b97a56b5dc33d7d21
DESKTOP-M5P3VPE\anoop <anoop09saini@gmail.com>
2020-08-07 01:27:11 -0500

    Pause Menu

---
+++ Vaporwave/Assets/ScriptFolder/PauseButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Pause();
+        }
+    }
+
+    public void Pause() {
+        if (GameIsPaused) {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameIsPaused = false;
+        } else {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+        }
+    }
+}
